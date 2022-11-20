f2b563a31c4f74b27f20767d9af336a02443e7ec
Levan Ilashvili <levan@ilashvili.me>
2022-11-20 13:17:18 +0400

    Added app bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { AppBar, Box, Grid, IconButton, Toolbar, Typography, withStyles } from '@mui/material';
+import { Container } from '@mui/system';
+import libreLogo from './assets/images/libre.svg';
+import Button from '@mui/material/Button';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+const styles = ({
+  logo: {
+    height: 40,
+  }
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+        <Toolbar>
+
+          <Grid component="div" sx={{ flexGrow: 1 }}>
+            <img src={libreLogo} style={styles.logo} alt="Libre Logo" />
+          </Grid>
+          <Button color="inherit">Connect</Button>
+        </Toolbar>
+        </AppBar>
+      </Box>
+      <main>
+      </main>
+    </ThemeProvider>
+
   );
 }
 
